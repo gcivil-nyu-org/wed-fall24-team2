45b7ea1556bccfc4efde01db004e1cd9b17bf136
grandinquisitor <dipeshparu@gmail.com>
2024-10-07 18:08:24 -0400

    Letting EB decide the DB, since the default db_name is unknown.

--- core/settings.py
+++ core/settings.py
@@ -88,25 +88,29 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'soundscape_psql',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'nyc_ss_db',
-    #     'USER': 'dp3754',
-    #     'PASSWORD': 'dp3754',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
-
-}
+#For local DB, enter your own creds:
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'nyc_ss_db',
+            'USER': 'dp3754',
+            'PASSWORD': 'dp3754',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
