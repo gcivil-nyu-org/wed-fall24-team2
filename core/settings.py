5ef947d5ed1412d40b7ea5075364d985b3434eed
changtyd <belle880126@gmail.com>
2024-10-08 02:18:23 -0400

    enable database configuration with env

--- core/settings.py
+++ core/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'chatroom',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': 5432
     }
