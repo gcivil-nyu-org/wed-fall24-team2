dfd0c8086b927705d1900764d82ef46b3f445dcd
Sonia Susanto <soniasusanto@gmail.com>
2024-10-11 17:59:52 -0400

    add simple chat ui

--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('data/', include('data_collection.urls')),
     path('', include('soundscape.urls')),
     path('admin/', admin.site.urls),
+    path('chatroom/', include('chatroom.urls'))
 ]
 
 if settings.DEBUG:
