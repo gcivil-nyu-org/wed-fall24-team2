aba7342b6e2715ce8b442e960289f231493b9119
Sonia Susanto <soniasusanto@gmail.com>
2024-10-16 21:24:41 -0400

    reformat

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Take environment variables from .env file
 env = environ.Env()
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # MAPBOX_ACCESS_TOKEN
