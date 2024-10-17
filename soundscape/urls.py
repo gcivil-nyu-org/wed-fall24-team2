b73dc257b7fb05717bc899d954f342075cd6c3c2
Yashwanth Alapati <145064639+yashwanth-alapati@users.noreply.github.com>
2024-10-17 19:19:39 -0400

    added login, signup, about.not yet connected to database (#80)
    * added login, signup, about.not yet connected to database
    
    * fixed black
    
    * fixed flake8 errors
    
    * removed about

--- soundscape/urls.py
+++ soundscape/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import LoginForm
 
 app_name = "soundscape"
+
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("about/", views.about, name="about"),
+    path("signup/", views.signup, name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="soundscape/login.html", authentication_form=LoginForm
+        ),
+        name="login",
+    ),
 ]
--- soundscape/views.py
+++ soundscape/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+from .forms import SignupForm
 import os
 
 
 def homepage(request):
     return render(
         request,
-        "homepage.html",
+        "soundscape/homepage.html",
         {"mapbox_access_token": os.environ.get("MAPBOX_ACCESS_TOKEN")},
     )
+
+
+def about(request):
+    return render(request, "soundscape/about.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("/login/")
+    else:
+        form = SignupForm()
+
+    return render(request, "soundscape/signup.html", {"form": form})
