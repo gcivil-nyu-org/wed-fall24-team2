2ad41932bcb4b1b8ff8ee7728cf51864d0171ebb
manavparikh01 <manavparikh01@gmail.com>
2024-10-18 15:31:18 -0400

    some changes

--- core/settings.py
+++ core/settings.py
@@ -110,18 +110,6 @@ WSGI_APPLICATION = "core.wsgi.application"
 #      }
 # }
 
-# if 'RDS_DB_NAME' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': os.environ['RDS_DB_NAME'],
-#             'USER': os.environ['RDS_USERNAME'],
-#             'PASSWORD': os.environ['RDS_PASSWORD'],
-#             'HOST': os.environ['RDS_HOSTNAME'],
-#             'PORT': os.environ['RDS_PORT'],
-#         }
-#     }
-# #For local DB, enter your own creds:
 if "RDS_DB_NAME" in os.environ:
     DATABASES = {
         "default": {
