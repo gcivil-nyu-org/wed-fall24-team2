8b3e09f0e92e51d119a9abfba2ee5202b1e1ccd6
Tzu Yi Chang <63236075+tzuyi126@users.noreply.github.com>
2024-10-19 16:42:59 -0400

    Create chatrooms by admin and display on map (#72)
    * write migrations script for chatrooms and superuser
    
    * add option setting for static to enable admin page on prod
    
    * enable csv loader for chatrooms creation
    
    * create chatrooms with csv and enable chatroom markers display
    
    * rearrange import
    
    * format with black
    
    * format with flake8
    
    * restore nav bar in homepage

--- soundscape/views.py
+++ soundscape/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
-
+from django.forms.models import model_to_dict
 
 from .forms import SignupForm
+from chatroom.models import Chatroom
+
 import os
+import json
 
 
 def homepage(request):
     return render(
         request,
         "soundscape/homepage.html",
-        {"mapbox_access_token": os.environ.get("MAPBOX_ACCESS_TOKEN")},
+        {
+            "mapbox_access_token": os.environ.get("MAPBOX_ACCESS_TOKEN"),
+            "chatrooms": json.dumps(
+                [model_to_dict(chatroom) for chatroom in Chatroom.objects.all()]
+            ),
+        },
     )
 
 
