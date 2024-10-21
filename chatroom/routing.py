092270fef433ebef2f7b50fff456462689e09983
grandinquisitor <dipeshparu@gmail.com>
2024-10-21 17:53:43 -0400

    final final commit

--- chatroom/routing.py
+++ chatroom/routing.py
@@ -1,22 +1,9 @@
-# from django.urls import path
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 from . import consumers
 
-# websocket_urlpatterns = [
-#     path('ws/chat/<str:chatroom_name>/', consumers.ChatRoomConsumer.as_asgi()),
-# ]
-
-# URLs that handle the WebSocket connection are placed here.
+# WebSocket URLs that handle the connection for chatrooms
 websocket_urlpatterns = [
     re_path(
         r"ws/chatroom/(?P<chatroom_name>\w+)/$", consumers.ChatRoomConsumer.as_asgi()
     ),
 ]
-
-application = ProtocolTypeRouter(
-    {
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-    }
-)
