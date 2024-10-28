90583851293f102497ff181ef7b86926a02b507f
changtyd <belle880126@gmail.com>
2024-10-28 17:17:43 -0400

    format with black

--- chatroom/management/commands/createsu.py
+++ chatroom/management/commands/createsu.py
@@ -7,8 +7,13 @@ class Command(BaseCommand):
     help = "Create a superuser"
 
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
-            User.objects.create_superuser(username=os.environ["DJANGO_SUPERUSER_USERNAME"], password=os.environ["DJANGO_SUPERUSER_PASSWORD"])
+        if not User.objects.filter(
+            username=os.environ["DJANGO_SUPERUSER_USERNAME"]
+        ).exists():
+            User.objects.create_superuser(
+                username=os.environ["DJANGO_SUPERUSER_USERNAME"],
+                password=os.environ["DJANGO_SUPERUSER_PASSWORD"],
+            )
             self.stdout.write(self.style.SUCCESS("Superuser admin has been created"))
         else:
             self.stdout.write(self.style.SUCCESS("Superuser admin already exists"))
