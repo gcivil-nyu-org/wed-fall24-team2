7b3abe8769d78cfe26e0276215fbf3c2fe454e05
manavparikh01 <manavparikh01@gmail.com>
2024-10-27 21:15:21 -0400

    putting user uploaded data into soundscap

--- soundscape/views.py
+++ soundscape/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.forms.models import model_to_dict
+from soundscape_user.models import SoundFileUser 
 
 from .forms import SignupForm
 from chatroom.models import Chatroom
@@ -24,6 +25,16 @@ def homepage(request):
     sound_type = request.GET.get("soundType", "Noise")
     date_from = request.GET.get("dateFrom")
     date_to = request.GET.get("dateTo")
+
+    # Query SoundFileUser data
+    user_sound_files = SoundFileUser.objects.all()
+    user_sound_files_data = json.dumps([model_to_dict(sound) for sound in user_sound_files])
+
+    print("Hello")
+    print(user_sound_files)
+
+    print("hello hello")
+    print(user_sound_files_data)
 
     try:
         batch_offsets = range(0, TOTAL_ROWS, BATCH_SIZE)
@@ -71,6 +82,7 @@ def homepage(request):
                 ),
                 "username": request.user.username,
                 "sound_data": json.dumps(all_data),
+                "user_sounds": user_sound_files_data,
             },
         )
 
@@ -86,6 +98,7 @@ def homepage(request):
                 "username": request.user.username,
                 "sound_data": json.dumps([]),  # Empty data on error
                 "error_message": str(e),
+                "user_sounds": user_sound_files_data,
             },
         )
 
