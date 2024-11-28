69278986bd183361875131b8dfc51ae8f0bbb848
Sonia Susanto <soniasusanto@gmail.com>
2024-11-28 10:05:44 -0500

    increase more

--- soundscape/views.py
+++ soundscape/views.py
@@ -94,8 +94,8 @@ def get_noise_data(request):
         APP_TOKEN = os.environ.get("NYC_OPEN_DATA_APP_TOKEN")
         headers = {"X-App-Token": APP_TOKEN} if APP_TOKEN else {}
 
-        TOTAL_RECORDS = 2500
-        BATCH_SIZE = 500
+        TOTAL_RECORDS = 5000
+        BATCH_SIZE = 1000
         MAX_WORKERS = 5  # Number of concurrent requests
 
         # Build the where clause
